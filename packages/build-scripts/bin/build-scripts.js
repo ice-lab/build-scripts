ea2e278504844b68ecd8c1a0edc3b39c32cc40e7
luhc228 <luhengchang228@gmail.com>
2020-11-04 10:54:55 +0800

    fix: comment

--- packages/build-scripts/bin/build-scripts.js
+++ packages/build-scripts/bin/build-scripts.js
@@ -4,6 +4,6 @@ const createCli = require('../lib/apis/create-cli');
 
 const scriptPath = require.resolve('./child-process-start.js');
 
-(async () => {
+(() => {
   createCli(() => [], scriptPath, packageInfo);
 })();
