1e738b29d9b21c1c06c1d4d09c3e4c0c436794c9
ClarkXia <xiawenwu41@gmail.com>
2021-07-19 11:30:11 +0800

    fix: add export of Context

--- packages/build-scripts/src/index.ts
+++ packages/build-scripts/src/index.ts
@@ -3,7 +3,8 @@ import start = require('./commands/start');
 import test = require('./commands/test');
 import WebpackService from './service/WebpackService';
 import JestService from './service/JestService';
+import Context from './core/Context';
 
 export * from './core/Context';
 export * from './types';
-export { build, start, test, WebpackService, JestService };
+export { build, start, test, WebpackService, JestService, Context };
