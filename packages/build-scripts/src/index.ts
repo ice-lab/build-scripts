5da9ac9628ff91a44f7089c7cd4e8501627c059d
ClarkXia <xiawenwu41@gmail.com>
2021-07-20 17:45:09 +0800

    fix: export apis

--- packages/build-scripts/src/index.ts
+++ packages/build-scripts/src/index.ts
@@ -4,7 +4,20 @@ import test = require('./commands/test');
 import WebpackService from './service/WebpackService';
 import JestService from './service/JestService';
 import Context from './core/Context';
+import webpackStart from './service/start';
+import webpackBuild from './service/build';
+import jestTest from './service/test';
 
 export * from './core/Context';
 export * from './types';
-export { build, start, test, WebpackService, JestService, Context };
+export {
+  build,
+  start,
+  test,
+  WebpackService,
+  JestService,
+  Context,
+  webpackStart,
+  webpackBuild,
+  jestTest,
+};
